5470e677875c6b48a636533e5d75731608f897f8
Gor G <gg@gorg.fr>
2024-05-18 15:42:58 +0200

    added pinging after the run is executed to run the code in app.before_request and initilize tts_service

--- lttss/cmd_impl.py
+++ lttss/cmd_impl.py
@@ -1,30 +1,79 @@
 import socket
 import requests
-import pyperclip
-import subprocess
-
-from config import LTTSSConfig 
+import threading
+import time
+import sys
 
 from os_api import OSAPI
 
 
+def start_server(port):
+    from tts_server_controller import app
+    app.run(port=port, host="localhost")
 
-def run(os_api : OSAPI, port : int):
+
+def run(os_api : OSAPI, port : int): # TODO : Clean this function
+
     try:
-        from tts_server_controller import app
-        app.run(port=port , host="localhost")
-    except Exception as e:
-        if isinstance(e, socket.gaierror):
+        response = requests.post(f"http://localhost:{port}/ping")
+        if response.text == "pong":
             response = requests.post(f"http://localhost:{port}/uname")
             if response.text == "lttss":
                 print(f"a lttss server is already running on port {port}.")
                 os_api.notify(f"a lttss server is already running on port {port}.")
+                return
             else:
                 print(f"Port {port} is occupied by some other process.")
                 os_api.notify(f"Port {port} is occupied by some other process.")
+                return
         else:
-            raise e
-    
+            raise Exception("Server did not respond to ping.")
+    except requests.exceptions.ConnectionError:
+        try:
+            server_thread = threading.Thread(target=start_server, args=(port,))
+            server_thread.start()
+
+            request_count = 0
+            max_request_count = 20
+            while request_count < max_request_count:
+                if server_thread.is_alive():
+                    try:
+                        response = requests.post(f"http://localhost:{port}/ping")
+                        if response.text != "pong":
+                            print("The server isn't responding to pings.")
+                        break
+                    except requests.exceptions.ConnectionError:
+                        time.sleep(.5)
+                        request_count += 1
+                else:
+                    print("The server process died unexpectedly.")
+                    break
+            if request_count == max_request_count:
+                raise Exception("Server did not start.")
+            else:
+                try:
+                    server_thread.join()
+                except KeyboardInterrupt: 
+                    max_wait = 10
+                    count = 0
+                    while server_thread.is_alive() and count < max_wait:
+                        time.sleep(1)
+                        count += 1
+
+                    if server_thread.is_alive():
+                        print("Server is taking too long to stop. Shutdown will be forcefull.")
+                    
+                    sys.exit(0)
+                    
+        except Exception as e:
+            if isinstance(e, socket.gaierror):
+                print(f"Port {port} is occupied by some other process.")
+                os_api.notify(f"Port {port} is occupied by some other process.")
+            else:
+                os_api.notify(f"An error occured while starting the server. \nMessage: {e}")
+                raise e
+        return
+
 
 
 def play_selected( os_api : OSAPI, port : int, lang : str):
